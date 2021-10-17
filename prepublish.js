27c173edb6156074f77c4a091b6537260f61547d
Vaibhav Shinde <vaibhavkshinde20@gmail.com>
2021-10-17 23:47:37 +0530

    fix(ci): add release config

--- prepublish.js
+++ prepublish.js
@@ -3,5 +3,6 @@ const pkg = require('./package.json');
 
 delete pkg['devDependencies'];
 delete pkg['scripts'];
+delete pkg['release'];
 
 fs.writeFileSync('./package.json', JSON.stringify(pkg, null, 2));
